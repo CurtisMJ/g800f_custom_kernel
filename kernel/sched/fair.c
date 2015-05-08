bb20a70f5a3c300c76552088fab81a0dd8780cab
Curtis J <curtismj.cj@gmail.com>
2015-05-08 19:04:28 +0200

    Linux 3.4.80

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2667,7 +2667,7 @@ static void destroy_cfs_bandwidth(struct cfs_bandwidth *cfs_b)
 	hrtimer_cancel(&cfs_b->slack_timer);
 }
 
-void unthrottle_offline_cfs_rqs(struct rq *rq)
+static void unthrottle_offline_cfs_rqs(struct rq *rq)
 {
 	struct cfs_rq *cfs_rq;
 
@@ -2726,7 +2726,7 @@ static inline struct cfs_bandwidth *tg_cfs_bandwidth(struct task_group *tg)
 	return NULL;
 }
 static inline void destroy_cfs_bandwidth(struct cfs_bandwidth *cfs_b) {}
-void unthrottle_offline_cfs_rqs(struct rq *rq) {}
+static inline void unthrottle_offline_cfs_rqs(struct rq *rq) {}
 
 #endif /* CONFIG_CFS_BANDWIDTH */
 
@@ -7229,6 +7229,9 @@ static void rq_online_fair(struct rq *rq)
 #endif
 
 	update_sysctl();
+
+	/* Ensure any throttled groups are reachable by pick_next_task */
+	unthrottle_offline_cfs_rqs(rq);
 }
 
 static void rq_offline_fair(struct rq *rq)
