e5f5aacf4c2d14e829e28e3fdedf6be06f06a7db
Curtis Jones <curtismj.cj@gmail.com>
2015-09-18 14:35:22 +0200

    Better way to do ramdisk

--- stock_ramdisk/sbin/initd.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh 
-export PATH=${PATH}:/system/bin:/system/xbin 
-mount -o remount rw /system 
-chmod 777 /system/etc/init.d/* 
-mount -o remount ro /system 
-for FILE in /system/etc/init.d/*; do 
-  sh $FILE 
-done
