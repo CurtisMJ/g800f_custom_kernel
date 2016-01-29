73036a8e1b0bb98c1262f32c641a65c2023d4520
CurtisMJ <curtismj.cj@gmail.com>
2016-01-29 16:31:51 +0200

    Revert "Resolutions"
    This reverts commit cdb2260cd43620f997a542d300c9c39fa825974b.

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -15,7 +15,6 @@
 
 #include <linux/ipv6.h>
 #include <linux/hardirq.h>
-#include <linux/jhash.h>
 #include <net/if_inet6.h>
 #include <net/ndisc.h>
 #include <net/flow.h>
@@ -408,17 +407,6 @@ struct ip6_create_arg {
 
 void ip6_frag_init(struct inet_frag_queue *q, void *a);
 int ip6_frag_match(struct inet_frag_queue *q, void *a);
-
-/* more secured version of ipv6_addr_hash() */
-static inline u32 __ipv6_addr_jhash(const struct in6_addr *a, const u32 initval)
-{
-	u32 v = (__force u32)a->s6_addr32[0] ^ (__force u32)a->s6_addr32[1];
-
-	return jhash_3words(v,
-			    (__force u32)a->s6_addr32[2],
-			    (__force u32)a->s6_addr32[3],
-			    initval);
-}
 
 static inline int ipv6_addr_any(const struct in6_addr *a)
 {
